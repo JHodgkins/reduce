44eb83d3386ef7a9d692e4883833d47aa1621d03
JHodgkins <jhodgkins78@gmail.com>
2022-04-12 21:21:50 +0000

    code: updated reduce() examples

--- reduce.js
+++ reduce.js
@@ -6,6 +6,26 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+// let sum = nums.reduce((acc, curr) => acc + curr);
+// console.log(sum); // result is 10
+
+// How we got 10
+let sum = nums.reduce((acc, curr) => {
+  console.log(
+    "accumalator:", acc,
+    "current value:", curr,
+    "total:", acc + curr
+  );
+  return acc + curr;
+}, 0); // 0 is the initial value
+console.log(sum);
+
+// simplified condensed version
+let sum2 = nums.reduce((acc, curr) => acc + curr, 0); // 0 is the initial value
+console.log("sum2", sum2);
+
+
 
 
 const teamMembers = [
@@ -28,10 +48,39 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'James',
+    profession: 'TechLead',
+    yrsExperience: 5
   }
 ];
 
 // Totaling a specific object property
-
+let totalExperiance = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log("Total Years Experiance:", totalExperiance);
 
 // Grouping by a property, and totaling it too
+// get profession and total years experiance between the people
+
+let experianceByProfession = teamMembers.reduce((acc, curr) => {
+  let theKey = curr.profession;
+  
+  if (!acc[theKey]) {
+    acc[theKey] = curr.yrsExperience;
+  } else {
+    acc[theKey] += curr.yrsExperience;
+  }
+  return acc;
+}, []);
+
+console.log(experianceByProfession);
+
+/**
+ * adding a new profession to the object list will
+ * automatically update
+ * {
+    name: 'James',
+    profession: 'TechLead',
+    yrsExperience: 5
+  } */
